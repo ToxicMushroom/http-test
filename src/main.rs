6f4513ea8fd8c47f4a57319d3b8cc9464554131c
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-08-05 20:17:59 +0200

    feat: turn into tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,50 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::thread;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    println!("{:?}", req);
-    format!("Hello {}!", &name)
+fn get_epoch_ms() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis()
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-        .bind(("0.0.0.0", 80))?
-        .run()
-        .await
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            // echo everything!
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(rep) => {
+            println!("An error occurred, terminating connection with {}\nError: {:?}", stream.peer_addr().unwrap(), rep);
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:80").unwrap();
+    // accept connections and process them, spawning a new thread for each one
+    println!("Server listening on port 80");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("{} New connection: {}", get_epoch_ms(), stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
 }
